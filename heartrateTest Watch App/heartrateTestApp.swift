c056f7dd80522258b1e7bd010a72a0279312ad5d
JaLuan Love <twgjay@gmail.com>
2023-07-31 14:34:54 -0700

    Initial Commit

---
+++ heartrateTest Watch App/heartrateTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  heartrateTestApp.swift
+//  heartrateTest Watch App
+//
+//  Created by TXT-76 on 7/31/23.
+//
+
+import SwiftUI
+
+@main
+struct heartrateTest_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
